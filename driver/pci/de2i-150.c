af48459e5dc89cfd6910434fb32cf96b7633f641
luanbezerra <luanbezerra@protonmail.com>
2025-08-08 23:21:08 -0300

    fix: simplify class creation by removing unnecessary module reference

--- driver/pci/de2i-150.c
+++ driver/pci/de2i-150.c
@@ -125,7 +125,7 @@ static int __init my_init(void)
 	printk("my_driver: device number %d was registered!\n", MAJOR(my_device_nbr));
 
 	/* 2. create class : appears at /sys/class */
-	if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+	if ((my_class = class_create(DRIVER_CLASS)) == NULL) {
 		printk("my_driver: device class count not be created!\n");
 		goto ClassError;
 	}
