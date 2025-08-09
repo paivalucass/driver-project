c7bb2d22562d81b8d26285b284ae7057a85292eb
luanbezerra <luanbezerra@protonmail.com>
2025-08-08 22:58:06 -0300

    refactor: optimize data handling in main loop

--- exemples/python/app-pci.py
+++ exemples/python/app-pci.py
@@ -25,15 +25,15 @@ def main():
         retval = os.write(fd, data.to_bytes(4, 'little'))
         print("wrote %d bytes"%retval)
 
+        ioctl(fd, RD_PBUTTONS)
+        red = os.read(fd, 4); # read 4 bytes and store in red var
+        print("red 0x%X"%int.from_bytes(red, 'little'))
         # data to write
-        data = 0x79404040;
+        data = red;
         ioctl(fd, WR_L_DISPLAY)
         retval = os.write(fd, data.to_bytes(4, 'little'))
         print("wrote %d bytes"%retval)
 
-        ioctl(fd, RD_PBUTTONS)
-        red = os.read(fd, 4); # read 4 bytes and store in red var
-        print("red 0x%X"%int.from_bytes(red, 'little'))
 
     os.close(fd)
 
